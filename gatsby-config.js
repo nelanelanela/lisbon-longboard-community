43455e9d0666d04db1f6fb052ff386c17ca78633
Natalia Terlecka <natalia@wehavesun.com>
2019-01-31 20:25:45 +0000

    Favicon added.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,41 @@ module.exports = {
       author: `@nelanelanela`,
   },
   plugins: [
+  // in gatsby-config.js
+  {
+    resolve: `gatsby-plugin-favicon`,
+    options: {
+      logo: "./src/images/favicon.png",
+
+      // WebApp Manifest Configuration
+      appName: null, // Inferred with your package.json
+      appDescription: null,
+      developerName: null,
+      developerURL: null,
+      dir: 'auto',
+      lang: 'en-US',
+      background: '#fff',
+      theme_color: '#fff',
+      display: 'standalone',
+      orientation: 'any',
+      start_url: '/?homescreen=1',
+      version: '1.0',
+
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false
+      }
+    }
+  }
+
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
