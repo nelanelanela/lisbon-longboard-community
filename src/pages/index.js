59b32f206ccbb362781719c188dbb865294cfe08
Natalia Terlecka <natalia@wehavesun.com>
2019-01-31 18:17:50 +0000

    Events added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,13 @@ const IndexPage = () => (
   <Layout>
     <h1>Hi 👋🏼</h1>
     <p>Welcome to the community of Lisbon Longboarders! We're hosting a meetup every two weeks. If you're from around or passing by Lisboa - join us!</p>
+    <h2>Next events:</h2>
+    <a href="https://www.facebook.com/events/374330423379728/">2.02.2019  Alcantara-Mar  15:00</a>
+    <p></p>
+    <a href="https://www.facebook.com/events/402676977173670/">16.02.2019  Alcantara-Mar  15:00</a>
+    <h2> </h2>
+    <h2> </h2>
+    <h2>See you there!</h2>
     <div style={{ maxWidth: `500px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
